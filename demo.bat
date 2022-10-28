f5f9d5ae1fc0aeb95f0bea4a1a080309521756fc
vidhya gaddam <68384240+vg185087@users.noreply.github.com>
2022-10-28 19:16:41 +0530

    sample

--- demo.bat
+++ demo.bat
@@ -3,13 +3,6 @@ echo %1
 echo $SOMEPASS
 echo $TEST
 echo %TEST:~1,2%
-
-if "%TEST%" == "abcdef"
-( echo "hello its working" )
-
-if $TEST == "abcdef" 
-( echo "hello its working with $" )
-
 set var=$SOMEPASS
 echo $var
 set sub=%var:~3,5%
