a19515645a42989a6da181f5cf60fe7403594926
vidhya gaddam <68384240+vg185087@users.noreply.github.com>
2022-10-28 18:56:11 +0530

    Update demo.bat

--- demo.bat
+++ demo.bat
@@ -4,9 +4,15 @@ echo $SOMEPASS
 echo $TEST
 echo %TEST:~1,2%
 
+@if "%TEST%"=="abcdef" @(
+        echo "hello its working"
+    )
+
+@if $TEST=="abcdef" @(
+        echo "hello its working with $"
+    )
+
 set var=$SOMEPASS
-echo %var
 echo $var
-echo %var%
 set sub=%var:~3,5%
 echo %sub%
